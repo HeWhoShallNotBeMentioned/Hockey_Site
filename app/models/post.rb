5025f9790aecb45ecd3786dbd752f6c6fee2091c
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2015-10-27 21:30:03 -0700

    edit spacing in tag.rb and add relationship to tags in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, :presence => true
   has_many :comments
+  has_and_belongs_to_many :tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,4 @@ class Tag < ActiveRecord::Base
   validates :name, presence: true
 
   has_and_belongs_to_many :posts
-
 end
