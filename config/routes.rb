e1bcf0809c659665f6c49ff9c6efcaa0ee54c810
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2015-10-29 15:20:27 -0700

    add baseline devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+
   resources :posts do
     resources :comments, only: :create
   end
