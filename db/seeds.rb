68f224da8885cd722fa37ff930dcfc4190652f0b
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2015-11-09 11:52:09 -0800

    info for seeding 6 users including 1 admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user_list = [
+  ["cunderwoodmn@gmail.com", "password", "admin", "true"],
+  ["cunderwoodmn@yahoo.com", "password", "Chris U"],
+  ["mail@gmail.com", "password", "W. Gretzky"],
+  ["Lemieux@hotmail.com", "password", "Le Magnifque"],
+  ["Mikita@prodigy.com", "password", "S.Mikita"],
+  ["S_Yzerman@AOL.com", "password", "Stevie_Y"],
+]
+
+user_list.each do |mail, pass, uname, admin|
+  User.create( email: mail, password: pass, username: uname, admin: admin )
+end
